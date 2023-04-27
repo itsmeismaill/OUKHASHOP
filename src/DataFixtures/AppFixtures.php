ae9337239804c79ef692782c69de75627226f653
itsmeismaill <ismail.oukha@etu.uae.ac.ma>
2023-04-27 16:42:18 +0100

    v2

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,41 @@
 
 namespace App\DataFixtures;
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
 
 class AppFixtures extends Fixture
 {
+   private UserPasswordHasherInterface $hasher;
+
+
+   public function __construct(UserPasswordHasherInterface $hasher){
+    $this->hasher=$hasher;
+   }
     public function load(ObjectManager $manager): void
     {
-        for ($i=0; $i <10 ; $i++) { 
-            # code...
+//         for ($i=0; $i <10 ; $i++) { 
+//             # code...
         
-         $product = new Product();
-        $product->setName('iphone X number'.$i)->setprice(260+$i*5)->setImgPath('images/it geek -34.png')->setDescription('Hadi description test etst ');
-        $manager->persist($product);
-}
+//          $product = new Product();
+//         $product->setName('iphone X number'.$i)->setprice(260+$i*5)->setImgPath('images/it geek -34.png')->setDescription('Hadi description test etst ');
+//         $manager->persist($product);
+// }
+         //users
+        for ($i=0; $i <5 ; $i++){
+          $user=new User();
+          $user->setFullName('ismail '.$i)
+            ->setPseudo('itsmeismaill' .$i)
+            ->setEmail('ismail.oukha'.$i.'@gmail.com')
+            ->setRoles(['ROLE USER'])
+            ->setPlainPassword('password');
+            $manager->persist($user);
+
+      }
         $manager->flush();
     }
 }
