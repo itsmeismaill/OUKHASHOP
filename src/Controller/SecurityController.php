6a1cba1b17264675df8e35e7267277327103af92
itsmeismaill <ismail.oukha@etu.uae.ac.ma>
2023-04-28 13:10:23 +0100

    v3

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,24 +2,59 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+Use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request ;
+
+
+
 class SecurityController extends AbstractController
 {
+    
     #[Route('/connexion', name: 'security.login', methods:['GET','POST'])]
      public function login(AuthenticationUtils $authenticationUtils): Response
-    {
+    { 
         // get the login error if there is one
          $error = $authenticationUtils->getLastAuthenticationError();
 
          // last username entered by the user
          $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('home.html.twig', [
+        return $this->render('/security/Login.html.twig', [
              'last_username' => $lastUsername,
              'error'         => $error,
         ]);
     }
+    #[Route('/deconnexion', name: 'security.logout', methods:['GET','POST'])] 
+    public function logout()
+    {
+
+    }
+    #[Route('/inscription', name: 'security.registration', methods:['GET','POST'])]
+    public function registration(Request $request,EntityManagerInterface $entityManager):Response
+    {
+        $user= new User();
+        $form= $this->createForm(RegistrationType::Class,$user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+           
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'The user '.$user->getFullName().' was Created successfully!'
+             );
+             return $this->redirectToRoute('home');
+            }
+        return $this->render('security/registration.html.twig',[
+            'form'=> $form->createView()
+        ]);
+    
+}
 }
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fullName',TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'minlenght'=>'2',
+                    'maxlenght'=>'50'
+                ],
+                'label'=>'Nom / Prenom ',
+                'label_attr'=>[
+                    'class'=>'form_label'
+                ],
+                'constraints'=>[
+                 new Assert\NotBlank(),
+                 new Assert\Length(['min'=>2 , 'max'=>50 ])
+                ]
+            ])
+            ->add('pseudo',TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'minlenght'=>'2',
+                    'maxlenght'=>'50'
+                ],
+                'label'=>'Pseudo (Facultatif)',
+                'label_attr'=>[
+                    'class'=>'form_label'
+                ],
+                'constraints'=>[
+                 new Assert\Length(['min'=>2 , 'max'=>50 ])
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'minlenght'=>'2',
+                    'maxlenght'=>'180'
+                ],
+                'label'=>'Email',
+                'label_attr'=>[
+                    'class'=>'form_label'
+                ],
+                'constraints'=>[
+                    new Assert\NotBlank(),
+                    new Assert\Email(),
+                 new Assert\Length(['min'=>2 , 'max'=>180])
+                ]
+            ])
+            ->add('plainPassword',RepeatedType::class,[
+                'type'=>PasswordType::class,
+                'first_options'=>[
+                  'attr'=>[
+                    'class'=>'form-control'
+                  ],
+                  'label'=>'Mot de passe',
+                  'label_attr'=>[
+                    'class'=>'form-label'
+                  ]
+                ],
+                'second_options'=>[
+                    'attr'=>[
+                        'class'=>'form-control'
+                      ],
+                      'label'=>'Mot de passe',
+                      'label_attr'=>[
+                        'class'=>'form-label'
+                      ]
+                ],
+                'invalid_message'=>'les mots de passes ne se correspondent pas.'
+            ])
+            ->add('Submit',SubmitType::Class,[
+                'attr'=>[
+                    'class'=>'btn btn-dark'
+                ]
+            ]);
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
